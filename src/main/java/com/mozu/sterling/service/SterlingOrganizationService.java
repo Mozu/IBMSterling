76832bb0c55a6eab8ccea2b78364a0993fbe8ffc
Lakshmi Nair <lakshmi_nair@volusion.com>
2015-11-24 14:54:12 -0600

    Code changes for tasks 73177, 73175 and 73176

--- src/main/java/com/mozu/sterling/service/SterlingOrganizationService.java
+++ src/main/java/com/mozu/sterling/service/SterlingOrganizationService.java
@@ -2,20 +2,31 @@ package com.mozu.sterling.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import javax.annotation.PostConstruct;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
-
+import com.mozu.api.ApiContext;
+import com.mozu.api.contracts.location.LocationType;
 import com.mozu.sterling.handler.ConfigHandler;
 import com.mozu.sterling.model.Setting;
 import com.mozu.sterling.model.organization.Organization;
 import com.mozu.sterling.model.organization.OrganizationList;
 import com.mozu.sterling.model.organization.ShipNode;
 import com.mozu.sterling.model.organization.ShipNodeList;
+import com.mozu.api.MozuApiContext;
+import com.mozu.api.contracts.core.Address;
+import com.mozu.api.contracts.location.Coordinates;
+import com.mozu.api.contracts.location.Location;
+import com.mozu.api.contracts.location.ShippingOriginContact;
+import com.mozu.api.resources.commerce.admin.LocationResource;
+import com.mozu.api.resources.commerce.admin.LocationTypeResource;
+import com.mozu.sterling.model.LocationMapping;
+
+
 
 @Service
 /**
@@ -29,12 +40,20 @@ public class SterlingOrganizationService extends SterlingClient {
 
     public final static String SHIP_NODE_SERVICE_NAME = "getShipNodeList";
     public final static String ORGANIZATION_LIST_SERVICE_NAME = "getOrganizationList";
+    private List<String> locationTypeList;
 
     @Autowired
     ConfigHandler configHandler;
 
     public SterlingOrganizationService() throws Exception {
         super();
+    }
+    
+    @PostConstruct
+    public void loadLocationTypes(){
+    	locationTypeList= new ArrayList<String>();
+    	locationTypeList.add("Store");
+    	locationTypeList.add("Warehouse");
     }
    
     /** 
@@ -89,5 +108,165 @@ public class SterlingOrganizationService extends SterlingClient {
         }
         return organizationList != null ? organizationList.getOrganization() : new ArrayList<>(); 
     }
+    
+    /**
+     * @param apiContext
+     * @param locationTypeCode
+     * @return
+     * @throws Exception
+     */
+    public LocationType createLocationType(ApiContext apiContext, String locationTypeCode) throws Exception{
+       	LocationType locationType = getLocationType(apiContext, locationTypeCode);
+       	if(locationType==null){
+       		locationType= new LocationType();
+       		locationType.setCode(locationTypeCode);
+       		locationType.setName(locationTypeCode);
+       		locationType = addLocationType(apiContext, locationType);
+       	}
+    	return locationType;
+    }
+    
+    /**
+     * @param shipNode
+     * @param apiContext
+     * @return
+     * @throws Exception
+     */
+    public Location createLocation(ShipNode shipNode, ApiContext apiContext ) throws Exception {
+    	Location location = getMozuLocation(shipNode.getNodeOrgCode(), apiContext);
+		if(location == null){
+    		mapLocation(apiContext,shipNode, new Location());	
+    		location=addMozuLocation(location,apiContext );
+		}else{
+			logger.info("Location with code "+location.getCode()+" already exists. So skipping the record");
+		}
+		return location;
+    	
+    }
+    
+    public Location mapLocation(ApiContext apiContext ,ShipNode shipNode, Location location) throws Exception{
+    	logger.info("Map Sterling location to Mozu location");
+    	
+    	location.setName(shipNode.getShipNode());
+		location.setDescription(shipNode.getDescription());
+		location.setCode(shipNode.getNodeOrgCode());
+		
+		Coordinates coordinates = new Coordinates();
+		coordinates.setLat(0);
+		coordinates.setLng(0);
+		location.setGeo(coordinates);
+		
+		Address locationAddress= new Address();
+		if(shipNode.getContactPersonInfo()!=null){
+			locationAddress.setAddress1(shipNode.getContactPersonInfo().getAddressLine1());
+			locationAddress.setAddress2(shipNode.getContactPersonInfo().getAddressLine2());
+			locationAddress.setAddress3(shipNode.getContactPersonInfo().getAddressLine3());
+			locationAddress.setAddress4(shipNode.getContactPersonInfo().getAddressLine4());
+			locationAddress.setCityOrTown(shipNode.getContactPersonInfo().getCity());
+			locationAddress.setCountryCode(shipNode.getContactPersonInfo().getCountry());
+			locationAddress.setPostalOrZipCode(shipNode.getContactPersonInfo().getZipCode());
+			locationAddress.setStateOrProvince(shipNode.getContactPersonInfo().getState());
+		}
+		location.setAddress(locationAddress);
+		
+		ShippingOriginContact shippingOriginContact = new ShippingOriginContact();
+		if(shipNode.getShipNodePersonInfo()!=null){
+			shippingOriginContact.setCompanyOrOrganization(shipNode.getContactPersonInfo().getCompany());
+			shippingOriginContact.setEmail(shipNode.getContactPersonInfo().getEMailID());
+			shippingOriginContact.setFirstName(shipNode.getContactPersonInfo().getFirstName());
+			shippingOriginContact.setLastNameOrSurname(shipNode.getContactPersonInfo().getLastName());
+			shippingOriginContact.setMiddleNameOrInitial(shipNode.getContactPersonInfo().getMiddleName());
+			String phoneNumber = shipNode.getContactPersonInfo().getDayPhone();
+			if(phoneNumber==null){
+				phoneNumber=shipNode.getContactPersonInfo().getEveningPhone();
+			}
+			if(phoneNumber==null){
+				phoneNumber = shipNode.getContactPersonInfo().getMobilePhone();
+			}
+			if(phoneNumber==null){
+				phoneNumber = shipNode.getContactPersonInfo().getOtherPhone();
+			}
+			
+			shippingOriginContact.setPhoneNumber(phoneNumber);
+		}
+		location.setShippingOriginContact(shippingOriginContact);
+		
+		List<LocationType> locationTypes = new ArrayList<LocationType>();
+		if(shipNode.getNodeType().equalsIgnoreCase("Store")){
+			LocationType locationType=new LocationType();
+			locationType.setCode("Store");
+	    	locationTypes.add(locationType);
+		}else{
+			LocationType locationType=new LocationType();
+			locationType.setCode("Warehouse");
+			locationTypes.add(locationType);
+		}
+		location.setLocationTypes(locationTypes);
+		return location;
+    }
+    
+    public void createAndAutoMapLocations(Integer tenantId) throws Exception{
+    	List<ShipNode> shipNodes = getShipNodes(tenantId);
+    	ApiContext apiContext = new MozuApiContext(tenantId);
+    	Setting setting = configHandler.getSetting(apiContext.getTenantId());
+    	List<LocationMapping> locationMappings = setting.getLocationMappings();
+    	for (ShipNode shipNode : shipNodes) {
+    		boolean mappingExists=false;
+    		Location location=createLocation(shipNode, apiContext);
+    		
+    		if(locationMappings == null){
+    			locationMappings = new ArrayList<LocationMapping>();
+    		}
+    		for (LocationMapping locationMapping : locationMappings) {
+				if(locationMapping.getLocationCode()!=null && locationMapping.getLocationCode().equalsIgnoreCase(location.getCode())){
+					mappingExists=true;
+					break;
+				}
+			}
+    		if(!mappingExists){
+	    		LocationMapping locationMapping = autoMapLocation(location);
+	    		locationMappings.add(locationMapping);
+    		}
+   	    }
+    	if(setting!=null){
+    		setting.setLocationMappings(locationMappings);
+    		configHandler.saveSettings(tenantId, setting);
+    	}
+    }
+    
+    public Location addMozuLocation(Location location , ApiContext apiContext) throws Exception{
+    	logger.info("Add location with code "+location.getCode()+" to Mozu");
+    	LocationResource locationResource = new LocationResource(apiContext);
+    	return locationResource.addLocation(location);
+    	
+    }
+    
+    public Location getMozuLocation(String locationCode , ApiContext apiContext) throws Exception{
+    	LocationResource locationResource = new LocationResource(apiContext);
+    	return locationResource.getLocation(locationCode);
+    	
+    }
+    
+    public LocationMapping autoMapLocation(Location location){
+    	LocationMapping locationMapping=new LocationMapping();
+    	locationMapping.setLocationCode(location.getCode());
+    	locationMapping.setShippingNodeCode(location.getCode());
+		return locationMapping;
+    }
+    
+    public LocationType addLocationType(ApiContext apiContext, LocationType locationType) throws Exception{
+    	LocationTypeResource locationTypeResource = new LocationTypeResource(apiContext);
+    	return locationTypeResource.addLocationType(locationType);
+    }
+    
+    public LocationType getLocationType(ApiContext apiContext, String locationTypeCode) throws Exception{
+    	LocationTypeResource locationTypeResource = new LocationTypeResource(apiContext);
+    	return locationTypeResource.getLocationType(locationTypeCode);
+    }
+    
+    public List<String> getLocationTypeList() {
+		return locationTypeList;
+	}
+    
 
 }
--- src/test/java/com/mozu/sterling/service/LocationServiceTest.java
+++ src/test/java/com/mozu/sterling/service/LocationServiceTest.java
@@ -1,28 +1,58 @@
 package com.mozu.sterling.service;
 
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertNotNull;
 import java.util.List;
-
+import com.mozu.api.MozuApiContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import com.mozu.api.contracts.location.Location;
+import com.mozu.api.ApiContext;
 import com.mozu.sterling.model.organization.ShipNode;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/servlet-context.xml"})
 public class LocationServiceTest {
+	protected static final int TENANT_ID = 4449;
+	protected static final int SITE_ID = 7280;
+	
     @Autowired
     SterlingOrganizationService locationService;
     
     @Test
     public void getShipNodesTest () throws Exception {
-        List<ShipNode> shipNodes = locationService.getShipNodes(419);
+        List<ShipNode> shipNodes = locationService.getShipNodes(TENANT_ID);
         
         assertTrue (shipNodes.size() > 0);
+    }
+    
+    @Test
+    public void createLocationTypeTest () throws Exception {
+    	ApiContext apiContext = new MozuApiContext(TENANT_ID);
+        List<String> locationTypeList = locationService.getLocationTypeList();
+        for (String locationTypeCode : locationTypeList) {
+        	assertNotNull(locationService.createLocationType(apiContext, locationTypeCode));
+		}
+    	
+   }
+    
+    @Test
+    public void createLocationTest () throws Exception {
+    	ApiContext apiContext = new MozuApiContext(TENANT_ID);
+    	  List<ShipNode> shipNodes = locationService.getShipNodes(TENANT_ID);
+    	  Location location=null;
+    	  for (ShipNode shipNode : shipNodes) {
+    		 location=locationService.createLocation(shipNode, apiContext);
+		  }
+    	  assertNotNull(location);
+    }
+    
+    @Test
+    public void createAndAutoMapLocationsTest() throws Exception{
+    	locationService.createAndAutoMapLocations(TENANT_ID);
     }
     
 }
