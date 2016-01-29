38b719050b7e8dad8bedf8898eeae2f0851dbeab
Fernando Cano <fernando_cano@volusion.com>
2016-01-29 12:10:41 -0600

    Integration test for topic listener and tweaks to support topics and queues.

--- src/integrationtest/java/com/mozu/sterling/service/SterlingQueueTest.java
+++ src/integrationtest/java/com/mozu/sterling/service/SterlingQueueTest.java
@@ -1,6 +1,9 @@
 package com.mozu.sterling.service;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +14,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.mozu.sterling.jmsUtil.DestinationTypeEnum;
 import com.mozu.sterling.jmsUtil.JmsConnectionStrategy;
 import com.mozu.sterling.jmsUtil.JmsResource;
 import com.mozu.sterling.model.Setting;
@@ -42,14 +47,13 @@ public class SterlingQueueTest {
 		setting = new Setting();
 		setting.setProviderEndpoint("50.23.47.110:7276:BootstrapBasicMessaging");
 		setting.setBusName("mozuJMS");
-		setting.setDestinationName("someTopic");
-		setting.setInboundDestinationName("someTopicIn");
+		setting.setDestinationName("t_ordercreate");
+		setting.setInboundDestinationName("t_ordercreate");
 
 		Integer tenantId = new Integer(15148);
 
-		NewSterlingToMozuOrderMessageListener listener = applicationContext
-				.getBean(NewSterlingToMozuOrderMessageListener.class);
-		listener.setTenantId(tenantId);
+		DefaultMessageListener listener = applicationContext
+				.getBean(DefaultMessageListener.class);
 
 		try {
 
@@ -57,7 +61,8 @@ public class SterlingQueueTest {
 					directConnectionStrategy.getConnectionFactory(setting),
 					directConnectionStrategy.getOutboundDestination(setting),
 					directConnectionStrategy.getInboundDestination(setting),
-					listener);
+					listener, DestinationTypeEnum.from(setting
+							.getDestinationType()));
 		} catch (JMSException e) {
 			logger.error("Failed getting jms resources.", e);
 		}
@@ -66,9 +71,10 @@ public class SterlingQueueTest {
 	@Test
 	public void readOrderFromQueue() throws Exception {
 
-		if (!jmsResource.isListening()) {
-			jmsResource.startListening();
-		}
+		 if (!jmsResource.isListening()) {
+		 jmsResource.startListening();
+		 }
 
+		Thread.sleep(10000); // wait for the listener to process the message
 	}
 }
--- src/main/java/com/mozu/sterling/jmsUtil/JmsResource.java
+++ src/main/java/com/mozu/sterling/jmsUtil/JmsResource.java
@@ -4,15 +4,17 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.MessageListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
-
 
 /**
  * Contains jms types needed to read and write to a single location.
  *
  */
 public class JmsResource {
+	private static final Logger logger = LoggerFactory.getLogger(JmsResource.class);
 
 	private JmsTemplate jmsTemplate;
 	private ConnectionFactory connectionFactory;
@@ -21,26 +23,31 @@ public class JmsResource {
 	private DefaultMessageListenerContainer listenerContainer;
 
 	public JmsResource(ConnectionFactory connectionFactory,
-			Destination destination, Destination readDestination, MessageListener listener) {
+			Destination destination, Destination readDestination,
+			MessageListener listener, DestinationTypeEnum destinationType) {
 		jmsTemplate = new JmsTemplate();
 		jmsTemplate.setConnectionFactory(connectionFactory);
 		jmsTemplate.setDefaultDestination(destination);
 
 		this.connectionFactory = connectionFactory;
 		this.defaultDestination = destination;
-		this.readDestination = readDestination;
+		this.readDestination = readDestination == null ? defaultDestination
+				: readDestination;
 
 		listenerContainer = new DefaultMessageListenerContainer();
 		listenerContainer.setConnectionFactory(connectionFactory);
+		listenerContainer.setDestination(this.readDestination);
 		listenerContainer
-				.setDestination(readDestination == null ? defaultDestination
-						: readDestination);
+				.setPubSubDomain(destinationType == DestinationTypeEnum.TOPIC);
 		listenerContainer.setAutoStartup(false);
+		listenerContainer.setCacheLevel(DefaultMessageListenerContainer.CACHE_NONE);
 		listenerContainer.setMessageListener(listener);
 	}
 
-	public JmsResource(ConnectionFactory connectionFactory, Destination destination, MessageListener listener) {
-		this(connectionFactory, destination, null, listener);
+	public JmsResource(ConnectionFactory connectionFactory,
+			Destination destination, MessageListener listener,
+			DestinationTypeEnum destinationType) {
+		this(connectionFactory, destination, null, listener, destinationType);
 	}
 
 	public JmsTemplate getJmsTemplate() {
@@ -54,12 +61,14 @@ public class JmsResource {
 	public void startListening() {
 		if (!listenerContainer.isRunning()) {
 			listenerContainer.start();
+			logger.info("JMS message listener container started!");
 		}
 	}
 
 	public void stopListening() {
 		if (listenerContainer.isRunning()) {
 			listenerContainer.stop();
+			logger.info("JMS message listener container stopped!");
 		}
 	}
 
