00df11d698c15cbefd2acc2e7c18c1bd83d509f1
Fernando Cano <fernando_cano@volusion.com>
2016-02-15 12:15:09 -0600

    unit text fix

--- src/integrationtest/java/com/mozu/sterling/service/SterlingQueueTest.java
+++ src/integrationtest/java/com/mozu/sterling/service/SterlingQueueTest.java
@@ -1,17 +1,11 @@
 package com.mozu.sterling.service;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-import javax.jms.Topic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,14 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jms.connection.SingleConnectionFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.test.context.ContextConfiguration;
 
-import com.ibm.websphere.sib.api.jms.JmsConnectionFactory;
-import com.ibm.websphere.sib.api.jms.JmsFactoryFactory;
-import com.ibm.websphere.sib.api.jms.JmsTopic;
 import com.mozu.sterling.jmsUtil.DestinationTypeEnum;
 import com.mozu.sterling.jmsUtil.JmsConnectionStrategy;
 import com.mozu.sterling.jmsUtil.JmsResource;
@@ -76,12 +63,13 @@ public class SterlingQueueTest {
 		setting.setInventoryDestinationName("q_agent");
 		setting.setDestinationType(DestinationTypeEnum.QUEUE.destinationName());
 
-		Integer tenantId = new Integer(419);
+		Integer tenantId = new Integer(15148);
+		Integer siteId = new Integer(21456);
 
 		try {
 			jmsResource = new JmsResource(
 					directConnectionStrategy.getJmsResourceSettings(setting,
-							tenantId));
+							tenantId, siteId));
 		} catch (JMSException e) {
 			logger.error("Failed getting jms resources.", e);
 		}
@@ -108,36 +96,6 @@ public class SterlingQueueTest {
 
 		Thread.sleep(30000); // wait for the listener to process the message
 
-//		String clientId = null;
-//		try {
-//			clientId = InetAddress.getLocalHost().getHostName();
-//		} catch (UnknownHostException e) {
-//			clientId = "unknownhost";
-//		}
 
-//		JmsFactoryFactory jmsFactoryFactory = JmsFactoryFactory.getInstance();
-//		JmsTopic topic = jmsFactoryFactory.createTopic("t_ordercreate");
-//
-//		ConnectionFactory connectionFactory = jmsResource.getConnectionFactory();
-//		JmsConnectionFactory sterlingCF = (JmsConnectionFactory)((SingleConnectionFactory)connectionFactory).getTargetConnectionFactory();
-//		sterlingCF.setClientID(clientId);
-//		sterlingCF.setDurableSubscriptionHome("smcfs94Node01.server1-mozuJMS");
-//
-//		Connection connection = connectionFactory.createConnection();
-//
-//		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//
-//		connection.start();
-//
-//
-//		session.createDurableSubscriber(topic, "testSubscription");
-//
-//		JmsTemplate template = jmsResource.getJmsTemplate();
-//
-//		logger.info("Subscription established");
-//
-//		connection.stop();
-//		session.close();
-//		connection.close();
 	}
 }
